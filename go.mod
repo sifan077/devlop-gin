a3c1cc28b412cdc42dba7e9b7d1c376e7927543d
思凡 <2262427313@qq.com>
2023-03-01 21:50:57 +0800

    http基础服务器

---
+++ go.mod
@@ -0,0 +1,1 @@
+module develop-gin
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"develop-gin/gee"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
+
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+
+	fmt.Println("127.0.0.1:9999 启动")
+	r.Run(":9999")
+}
